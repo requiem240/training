7bf75ae3260d9631ef4e94611eef9306aa2c699d
requiem240 <linden.friesen@gmail.com>
2016-11-01 10:47:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  count                  = "2"
+  count                  = "1"
   ami                    = "ami-c9580bde"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-3135ea1c"
